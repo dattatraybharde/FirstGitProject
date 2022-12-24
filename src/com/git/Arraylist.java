c13de3906d22f8aaf5a936a32805c168b9abb668
dattatraybharde <bharded143@gmail.com>
2022-12-24 17:48:14 +0530

    firstComment : added first code

---
+++ src/com/git/Arraylist.java
@@ -0,0 +1,17 @@
+package com.git;
+
+import java.util.ArrayList;
+
+public class Arraylist {
+
+	public static void main(String[] args) {
+		ArrayList<String> al=new ArrayList<String>();
+		al.add("kiran");
+		al.add("datta");
+		al.add("satyavan");
+		System.out.println(al);
+		
+
+	}
+
+}
